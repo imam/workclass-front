ac36769e6551ba86a2abf82d4d59c712837558f7
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2021-07-02 22:26:20 +0700

    Different treatment on different database bugfix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,17 @@ const { Title } = Typography;
 const CustomHits = connectHits(({ hits }) => {
   return <div className="flex flex-wrap justify-center">
     {hits.map(hit => {
+		//There is different treatment on date when pushing to Algolia
+		//Between postgresql and sqlite: PGSQL will push as date string
+		//SQLite will push as UNIX Timestamp
+		//Here we will check if it's a number or not, and if it is, then it must be
+		// UNIX Timestamp
 		//To convert from unix timestamp to Javascript date
 		// We need to multiply the unix number by 1000
 		// Refer: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-		hit.activation_date = hit.activation_date * 1000
+		if(Number.isInteger(hit.activation_date)){
+			hit.activation_date = hit.activation_date * 1000
+		}
 		return <div className="m-[10px]"><Link to={`/jobs/${hit.job_id}`}>
 			<JobCard job={hit} />
 		</Link></div>
